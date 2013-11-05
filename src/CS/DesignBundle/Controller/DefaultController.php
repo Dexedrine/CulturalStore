7a42ae122d7e11990445c0271897b6fffcbb0c5b
lootss <lucille.dhaleine@gmail.com>
2013-11-05 16:17:43 +0100

    fichiers app

--- src/CS/DesignBundle/Controller/DefaultController.php
+++ src/CS/DesignBundle/Controller/DefaultController.php
@@ -1,13 +1,12 @@
 <?php
 
 namespace CS\DesignBundle\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
-{
-    public function indexAction($name)
-    {
-        return $this->render('CSDesignBundle:Default:index.html.twig', array('name' => $name));
-    }
+class DefaultController extends Controller {
+	public function indexAction($name) {
+		return $this
+				->render('CSDesignBundle:Default:index.html.twig',
+						array('name' => $name));
+	}
 }
