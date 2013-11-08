4aab3a1c6f0d99e4699e18585a8047dce5e57f7e
gzk <florian.licour@gmail.com>
2013-11-08 01:49:17 +0100

    modifiation du formulaire d'inscription

--- src/CS/UserBundle/Entity/Utilisateur.php
+++ src/CS/UserBundle/Entity/Utilisateur.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace CS\UserBundle\Entity;
-
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -9,42 +8,103 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Entity
  * @ORM\Table(name="utilisateur")
  */
-class Utilisateur extends BaseUser
-{
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="integer")
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    protected $id;
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=30, nullable=false)
-     */
-    private $nom;
+class Utilisateur extends BaseUser {
+	/**
+	 * @ORM\Id
+	 * @ORM\Column(type="integer")
+	 * @ORM\GeneratedValue(strategy="AUTO")
+	 */
+	protected $id;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=30, nullable=false)
+	 */
+	private $nom;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=30, nullable=false)
-     */
-    private $prenom;
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=30, nullable=false)
+	 */
+	private $prenom;
 
+	/**
+	 * @var string
+	 *
+	 * @ORM\Column(type="string", length=300, nullable=true)
+	 */
+	private $photo;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=300, nullable=true)
-     */
-    private $photo;
+	/**
+	 * Set nom
+	 *
+	 * @param string $nom
+	 * @return Utilisateur
+	 */
+	public function setNom($nom) {
+		$this->nom = $nom;
 
+		return $this;
+	}
 
+	/**
+	 * Get nom
+	 *
+	 * @return string
+	 */
+	public function getNom() {
+		return $this->nom;
+	}
 
-
-    public function __construct()
-    {
-        parent::__construct();
-        // your own logic
-    }
+	/**
+	 * Set prenom
+	 *
+	 * @param string $prenom
+	 * @return Utilisateur
+	 */
+	public function setPrenom($prenom)
+	{
+		$this->prenom = $prenom;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get prenom
+	 *
+	 * @return string
+	 */
+	public function getPrenom()
+	{
+		return $this->prenom;
+	}
+	
+	/**
+	 * Set photo url
+	 *
+	 * @param string $photo
+	 * @return Utilisateur
+	 */
+	public function setPhoto($photo)
+	{
+		$this->photo = $photo;
+	
+		return $this;
+	}
+	
+	/**
+	 * Get photo
+	 *
+	 * @return string
+	 */
+	public function getPhoto()
+	{
+		return $this->photo;
+	}
+	
+	public function __construct() {
+		parent::__construct();
+		// your own logic
+	}
 }
