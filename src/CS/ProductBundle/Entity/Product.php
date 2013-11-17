738340613b4ff03a72a510afb3677a94ba2393bb
lucille <lucille@ubuntu>
2013-11-17 02:00:44 +0100

    ajout de bundles pour la gestion des produits

---
+++ src/CS/ProductBundle/Entity/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+// src/App/AppBundle/Entity/Product.php
+namespace CS\ProductBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Bundle\CartBundle\Model\CartItem;
+use Sylius\Bundle\OrderBundle\Model\OrderItemInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="cart_product")
+ *
+ *
+ */
+
+class Product extends CartItem implements OrderItemInterface
+{
+    // Your code...
+
+    public function getSellableName()
+    {
+        // Here you just have to return the nice display name of your merchandise.
+        return $this->name;
+    }
+}
