37d106fe531f286b62e973c51643fec6dd7f126c
lootss <lucille.dhaleine@gmail.com>
2013-12-02 18:18:31 +0100

    Test de l'ajout d'un produit (répare build)

--- src/CS/ProductBundle/Tests/ProductControllerTest.php
+++ src/CS/ProductBundle/Tests/ProductControllerTest.php
@@ -5,14 +5,45 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use CS\ProductBundle\Controller\ProductController;
 
 class ProductControllerTest extends WebTestCase{
+	
+	private $em;
+	
 	public function setUp() {
-		$pc = new ProductController;
+		static::$kernel = static::createKernel();
+		static::$kernel->boot();
+		$this->em = static::$kernel->getContainer()
+		->get('doctrine')
+		->getManager()
+		;
+		
+		
+		
+		
 	}
 	
 	public function testAddProperty() {
-		$product = $pc->repository->createNew();
-		$pc->addProperty($product,"price",10);
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/product/new/book');
+		$form = $crawler->selectButton('_submit')->form(array(
+				'form[name]'  => "test",
+				'form[description]'  => "test",
+				'form[price]'  => 10,
+				'form[image]'  => "test",
+				'form[genre]'  => "test",
+				'form[langue]'  => "test",
+				'form[type]'  => "test",
+				'form[format]'  => "test",
+				'form[auteur]'  => "test",
+				
+		));
+		$client->submit($form);
 		
-		$this->assertEquals($product->getProperties()["price"], 10);
+		$products = $this->em 
+			->getRepository('CSProductBundle:Product')
+			->findOneBy(array('name' => "test"))	
+		;
+		
+		$this->assertCount(1,$products);
+		//$this->assertEquals($product->getPropertyByName("price"), 10);
 	}
 }
