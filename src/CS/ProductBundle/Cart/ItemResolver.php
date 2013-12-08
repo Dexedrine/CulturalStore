91a4d8fb2a8d10013309e14eb40d8ee16f6c1f20
lucille <lucille@ubuntu>
2013-12-08 21:59:55 +0100

    début d'intégration du panier

--- src/CS/ProductBundle/Cart/ItemResolver.php
+++ src/CS/ProductBundle/Cart/ItemResolver.php
@@ -1,11 +1,12 @@
 <?php
 
 // src/App/AppBundle/Cart/ItemResolver.php
-namespace App\AppBundle\Cart;
+namespace CS\ProductBundle\Cart;
 
 use Sylius\Bundle\CartBundle\Model\CartItemInterface;
 use Sylius\Bundle\CartBundle\Resolver\ItemResolverInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManager;
 
 class ItemResolver implements ItemResolverInterface
 {
@@ -22,7 +23,7 @@ class ItemResolver implements ItemResolverInterface
     	
     	// If no product id given, or product not found, we throw exception with nice message.
     	if (!$productId || !$product = $this->getProductRepository()->find($productId)) {
-    		throw new ItemResolvingException('Requested product was not found');
+    		//throw new Exception('Requested product was not found');
     	}
     	
     	// Assign the product to the item and define the unit price.
@@ -35,6 +36,6 @@ class ItemResolver implements ItemResolverInterface
 
     private function getProductRepository()
     {
-        return $this->entityManager->getRepository('AppBundle:Product');
+        return $this->entityManager->getRepository('CSProductBundle:Product');
     }
 }
