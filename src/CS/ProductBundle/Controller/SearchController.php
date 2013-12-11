427f4cf5b1c00c5363fc690769598a94e335fab2
gzk <florian.licour@gmail.com>
2013-12-11 15:46:45 +0100

    résultat recherche multiple

--- src/CS/ProductBundle/Controller/SearchController.php
+++ src/CS/ProductBundle/Controller/SearchController.php
@@ -10,89 +10,28 @@ class SearchController extends Controller {
 
 	public function searchAction(Request $request) {
 
-		$form = $this->createFormBuilder()->add('Recherche', 'text')
+		$form = $this->createFormBuilder()->add('recherche', 'text')
 				->add('Rechercher', 'submit')->getForm();
 
 		$form->handleRequest($request);
 
 		if ($form->isValid()) {
 			//récupère les résultats de la recherche
-// 			$userType = $this->container
-// 					->get('fos_elastica.index.website.product');
-// 			/** var FOQ\ElasticaBundle\Manager\RepositoryManager */
 			$repositoryManager = $this->container->get('fos_elastica.manager');
-			
-			/** var FOQ\ElasticaBundle\Repository */
-			$repository = $repositoryManager->getRepository('CSProductBundle:Product');
-			
-			/** var array of Acme\UserBundle\Entity\User */
-			$products = $repository->find('test');
-			
-			//$hybridResults = $finder->findHybrid('test');
-			foreach ($products as $product) {
+			$repository = $repositoryManager
+					->getRepository('CSProductBundle:Product');
+			$products = $repository
+					->find($form["recherche"]->getData());
+			return $this
+					->render(
+							'CSProductBundle:Product:search_result.html.twig',
+							array('products' => $products,
+									'recherche' => $form["recherche"]->getData(),));
 
-				return $this
-				->redirect(
-						$this
-						->generateUrl('sylius_product_show',
-								array('id' => $product->getId(),)));
-			}
-			
 		}
 		return $this
 				->render('CSProductBundle:Product:search.html.twig',
 						array('form' => $form->createView(),));
-		// 		$form = $this->createFormBuilder()
-		// 			->add('search', 'text')
-		// 			->getForm();
-
-		// 		if ($request->isMethod('POST')) {
-		// 			$form->bind($request);
-
-		// 			if ($form->isValid()) {
-
-		// 				$query = $request->get('search', null);
-
-		// 				// notre index est directement disponible sous forme de service
-		// 				$index = $this->container
-		// 						->get('fos_elastica.index.website.user');
-
-		// 				$searchQuery = new \Elastica\Query\QueryString();
-		// 				$searchQuery->setParam('query', $query);
-
-		// 				// nous forçons l'opérateur de recherche à AND, car on veut les résultats qui
-		// 				// correspondent à tous les mots de la recherche, plutôt qu'à au moins un
-		// 				// d'entre eux (opérateur OR)
-		// 				$searchQuery->setDefaultOperator('OR');
-
-		// 				// on exécute une requête de type "fields", qui portera sur les colonnes "name"
-		// 				// et "zipcode" de l'index
-		// 				$searchQuery->setParam('fields', array('nom', 'prenom',));
-
-		// 				// exécution de la requête, limitée aux 10 premiers résultats
-		// 				$results = $index->search($searchQuery, 10)->getResults();
-
-		// 				$data = array();
-
-		// 				// on arrange les données des résultats...
-		// 				foreach ($results as $result) {
-		// 					$source = $result->getSource();
-		// 					$data[] = array(
-		// 							'suggest' => $source['prenom'] . ' '
-		// 									. $source['nom'],
-		// 							'prenom' => $source['prenom'],
-		// 							'nom' => $source['nom'],);
-		// 				}
-
-		// 				// ...avant de les retourner en json
-		// 				return new JsonResponse($data, 200,
-		// 						array('Cache-Control' => 'no-cache',));
-		// 			}
+		// 		pour l'autocompletion = http://devblog.lexik.fr/symfony2/autocompletion-avec-elasticsearch-2525
 	}
-
-	// 		return $this
-	// 				->render('CSProductBundle:Product:search.html.twig');
-
-	// 	}
-
 }
