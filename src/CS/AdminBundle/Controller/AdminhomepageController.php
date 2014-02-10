40ff7eb5460f28e77c7417259fec7a8fdbd9c5ef
dexedrine <lumu59@gmail.com>
2014-02-10 18:53:19 +0100

    affichage de l'ensemble des fournisseurs enabled=false et validation côté administrateur

--- src/CS/AdminBundle/Controller/AdminhomepageController.php
+++ src/CS/AdminBundle/Controller/AdminhomepageController.php
@@ -10,4 +10,44 @@ class AdminhomepageController extends Controller {
 		return $this
 				->render('CSAdminBundle:Homepage:homepageAdmin.html.twig', array('user'=>$user));
 	}
+	
+	public function showFournisseursAValiderAction(){
+		$repository = $this->getDoctrine()->getRepository('CSUserBundle:Fournisseur');
+		$fournisseurs = $repository->findBy(array('enabled' => false));
+		
+		return $this
+			->render('CSAdminBundle:Validate:validateFournisseur.html.twig', array('fournisseurs'=>$fournisseurs));		
+	}
+	
+	public function validateFournisseurAction($id_fournisseur){
+		$em = $this->getDoctrine()->getManager();
+		$repository = $this->getDoctrine()->getRepository('CSUserBundle:Fournisseur');
+		$fournisseur = $repository->findOneById($id_fournisseur);
+		
+		$fournisseur->setEnabled(true);
+		$em->flush();		
+		return $this->redirect($this->generateUrl('cs_admin_show_fournisseur'));
+		
+	}
+	
+	public function showCommunityAValiderAction(){
+		$repository = $this->getDoctrine()->getRepository('CSCommunityBundle:Community');
+		$communities = $repository->findBy(array('enabled' => false));
+		
+		
+		return $this
+		->render('CSAdminBundle:Validate:validateCommunity.html.twig', array('communities'=>$communities));
+	}
+	
+	public function validateCommunityAction($id_fournisseur){
+		$em = $this->getDoctrine()->getManager();
+		/*$repository = $this->getDoctrine()->getRepository('CSUserBundle:Fournisseur');
+		$community = $repository->findOneById($id_fournisseur);
+	
+		$fournisseur->setEnabled(true);
+		$em->flush();*/
+	
+		return $this
+		->render('CSAdminBundle:Validate:validateFournisseur.html.twig', array('fournisseurs'=>$fournisseurs));
+	}
 }
